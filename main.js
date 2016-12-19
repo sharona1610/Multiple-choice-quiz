75be4a1ff9fbc5f989cb2dfbcf909d8ee27ec1d9
sharona1610 <sharona1610@gmail.com>
2016-12-19 20:12:28 +0800

    add game logic

--- main.js
+++ main.js
@@ -5,9 +5,10 @@ function question(prompt, options, correctAnswer){
 }
 var quiz={
   questions: [],
+  playTurn: 1,
   isGameOver: false,
   currentQuestion: 0,
-  player1Ppoints: 0,
+  player1Points: 0,
   player2Points: 0
 }
 
@@ -31,5 +32,68 @@ temp = new question("Jane: 'What ..... in the evenings?' Mary: 'Usually I watch 
 quiz.questions.push(temp)
 temp = new question("	Jane: 'What ..... ?' Mary: 'I'm trying to fix my calculator.'", ['	you doing', 'you do', 'do you do', 'are you doin'], 4)
 quiz.questions.push(temp)
+console.log(quiz.questions[0].options[0]);
 
-console.log(quiz.questions);
+$('.start').click(function(){
+  if(!quiz.isGameOver)
+    gameStatus()
+})
+
+function gameStatus(){
+  if(quiz.currentQuestion<10){
+    console.log("ok");
+    game()
+  }
+  else{
+    quiz.isGameOver= true
+    console.log("over");
+    gameOver()
+  }
+}
+
+function gameOver(){
+  // console.log(quiz.player1Points);
+  // console.log(quiz.player2Points);
+}
+function game(){
+  if(!quiz.isGameOver){
+  $('.quest').text(quiz.questions[quiz.currentQuestion].prompt)
+  $('.opt1').text(quiz.questions[quiz.currentQuestion].options[0])
+  $('.opt2').text(quiz.questions[quiz.currentQuestion].options[1])
+  $('.opt3').text(quiz.questions[quiz.currentQuestion].options[2])
+  $('.opt4').text(quiz.questions[quiz.currentQuestion].options[3])
+}
+}
+
+$('.radio').change(function(){
+  if(parseInt(this.id)===quiz.questions[quiz.currentQuestion].correctAnswer){
+    if(quiz.playTurn===1){
+      quiz.player1Points+=1;
+      $('.ply1_score').text("Player 1: "+ quiz.player1Points)
+    }
+    else{
+      quiz.player2Points+=1
+      $('.ply2_score').text("Player 2: "+ quiz.player2Points)
+    }
+  }
+  else {
+    // console.log("wrong");
+  }
+  if(quiz.playTurn===1){
+    quiz.playTurn=2
+  }
+  else{
+    quiz.playTurn=1
+  }
+  quiz.currentQuestion+=1
+  this.checked=false;
+  gameStatus()
+})
+$('.reset').click(function(){
+  quiz.playTurn= 1,
+  quiz.isGameOver= false,
+  quiz.currentQuestion= 0,
+  quiz.player1Points= 0,
+  quiz.player2Points= 0
+  gameStatus()
+})
